25d9c6e48719ab55d15cbb130d7b8a8cf163064a
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-03-04 17:40:49 +0530

    FT2-37-1: Added further checks for validating user input. Description: Confined the length of input. Handled blankspace entry in the input field.

--- Assign1/index.php
+++ Assign1/index.php
@@ -1,37 +1,47 @@
 <?php
-    $errMsg="";
-    $greetings;
-    $returnMsg;
-    if(isset($_POST["submit"])){
-        $fname= $_POST['fname'];
-        $lname= $_POST['lname'];
+    if(isset($_POST["submit"])) {
+        //fname, lname values hold the first and last name.
+        $fname= trim($_POST['fname']," ");
+        $lname= trim($_POST['lname'], " ");
+        //New object created.
         $user = new User($fname,$lname);
+
+        //returnMsg stores the value determining if invalid entry is done in input fields.
         $returnMsg= $user->isValid();
         if($returnMsg==1)
-            $errMsg= "*Only alphabets allowed";
-        else if(!empty($returnMsg))
+            $ferrMsg= "*Only alphabets allowed";
+        elseif($returnMsg==2)
+            $lerrMsg = "*Only alphabets allowed";
+        else
             $greetings= $returnMsg;
     }
-    class User{
+    class User {
         public $fname, $lname;
-        public function __construct($fname, $lname){
+        public function __construct($fname, $lname) {
             $this->fname = $fname;
             $this->lname = $lname;
         }
-        public function isValid(){
-            $greetings="";
-            if (!preg_match("/^[a-zA-Z ]+$/",$this->fname))
+
+        /*
+        Checks if the input name follows the general naming pattern,
+        and returns the message accordingly.
+        */
+        public function isValid() {
+            // $pattern = "/^[a-zA-Z]+[ ]*[a-zA-Z]+$/";
+            $pattern = "/^[a-zA-Z]+$/";
+            if (!preg_match($pattern, $this->fname))
                 return 1;
-            else if (!preg_match("/^[a-zA-Z ]+$/",$this->lname))
-                return 1;
-            else
-            {
+            else if (!preg_match($pattern, $this->lname))
+                return 2;
+            else {
                 $greetings = "Hello $this->fname $this->lname";
                 return $greetings;
             }
         }
     }
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -91,13 +101,16 @@
         <div class="container">
             <h2>USER DETAILS</h2>
             <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post" class="input-form">
-                First Name: <input type="text" name="fname" placeholder="First Name"><br>
-                Last Name: <input type="text" name="lname" placeholder="Last Name"><br>
-                Full Name: <input type="text" name="fullName"placeholder="Full Name" value ="<?php if($error!=1)echo $fname." ".$lname?>"disabled><br>
+                First Name: <input type="text" name="fname" placeholder="First Name" value="<?php echo $fname?>" maxlength=25><br>
+                <span class="error"><?php if($returnMsg==1)echo $ferrMsg;?></span><br>
+
+                Last Name: <input type="text" name="lname" placeholder="Last Name" value="<?php echo $lname?>" maxlength=25><br>
+                <span class="error"><?php if($returnMsg==2)echo $lerrMsg;?></span><br>
+
+                Full Name: <input type="text" name="fullName"placeholder="Full Name" value ="<?php if($greetings!="")echo $fname." ".$lname?>"disabled><br>
                 <input  class="submit-button" type="submit" name="submit" value="Submit"> 
             </form>
-            <span class="greetings"><?php echo $greetings;?></span>
-            <span class="error"><?php if($errMsg!="")echo $errMsg;?></span>
+            <span class="greetings"><?php echo $greetings;?></span>     
         </div>
     </section>
 </body>
