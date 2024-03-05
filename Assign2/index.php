5f03d13e9e074cba9d3cad3d7f3ef336b628f35f
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-03-05 15:22:54 +0530

    FT2-37-2: Made changes in the indendation.

--- Assign2/index.php
+++ Assign2/index.php
@@ -1,59 +1,31 @@
 <?php
-$errMsg = "";
-$greetings;
-$returnMsg;
-if (isset($_POST["submit"])) {
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $user = new User($fname, $lname);
-    $returnMsg = $user->isValid();
-    if ($returnMsg == 1)
-        $errMsg = "*Only alphabets allowed";
-    else if (!empty($returnMsg)){
-        $greetings = $returnMsg;
-        if (isset($_FILES["image"])){
-            $imgPath = $user->isValidImage();
+
+    include 'User.php';
+    if (isset($_POST["submit"])) {
+        // Variables fname and lname hold the first and last name.
+        $fname = trim($_POST['fname']);
+        $lname = trim($_POST['lname']);
+        $fullName = $_POST['fullName'];
+        if (empty($fullName)) {
+            // New object created.
+            $user = new User($fname, $lname);
+
+           // Variable returnMsg stores the value determining if invalid entry is done in input fields.
+            $returnMsg = $user->isValid();
+            if ($returnMsg == 1)
+                $errMsg = "*Only alphabets allowed";
+            else if (!empty($returnMsg)) {
+                $greetings = $returnMsg;
+                if (isset($_FILES["image"])) {
+                    $imgPath = $user->isValidImage();
+                }
+            }
         }
-    }
-}
-class User
-{
-    public $fname, $lname;
-    public function __construct($fname, $lname)
-    {
-        $this->fname = $fname;
-        $this->lname = $lname;
-    }
-    public function isValid()
-    {
-        $this->fname =$this->test_input($this->fname);
-        $this->lname =$this->test_input($this->lname);
-        $greetings = "";
-        if (!preg_match("/^[a-zA-Z ]+$/", $this->fname))
-            return 1;
-        else if (!preg_match("/^[a-zA-Z ]+$/", $this->lname))
-            return 1;
         else {
-            $greetings = "Hello $this->fname $this->lname";
-            return $greetings;
+            $err = "Can't edit full name";
         }
     }
-    public function isValidImage()
-    {
-        $file = $_FILES["image"];
-        $targetDir = "uploads/";
-        $target = $targetDir . basename($_FILES['image']['name']);
-        $tempFileName = $_FILES['image']['tmp_name'];
-        if (move_uploaded_file($tempFileName, $target)) {
-            return $target;       
-        }
-    }
-    public function test_input($data) {
-        $data = trim($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -72,22 +44,23 @@ class User
         <div class="container">
             <h2>USER DETAILS</h2>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="input-form"enctype="multipart/form-data">
-                First Name: <input type="text" name="fname" placeholder="First Name"><br>
-                Last Name: <input type="text" name="lname" placeholder="Last Name"><br>
-                Full Name: <input class="fname" type="text" name="fullName" placeholder="Full Name"
-                    value="<?php if ($error != 1)
-                        echo $fname . " " . $lname ?>" disabled><br>
-                    Upload your image <input class="image-input" type="file" name="image"><br>
-                    <input class="submit-button" type="submit" name="submit" value="Submit">
+                First Name: <input type="text" name="fname" placeholder="First Name" value="<?php echo $fname?>" maxlength=25 pattern="^[a-zA-Z]+$" required><br>
+                Last Name: <input type="text" name="lname" placeholder="Last Name" value="<?php echo $lname?>" maxlength=25 pattern="^[a-zA-Z]+$" required><br>
+                Full Name: <input class="fname" type="text" name="fullName" placeholder="Full Name" value="<?php if ($greetings != "") echo $fname . " " . $lname ?>" disabled><br>
+                <span class="error">
+                    <?php if (!empty($err)) echo $err; ?>
+                </span><br>
+                Upload your image <input class="image-input" type="file" name="image"><br>
+                <input class="submit-button" type="submit" name="submit" value="Submit">
             </form>
             <div class="greetings-wrapper">
                 <span class="greetings">
-                    <?php echo $greetings; ?><br>
+                    <?php if (!empty($greetings)) echo $greetings; ?><br>
                     <img src="./<?php echo $imgPath?>" alt="">
                 </span>
             </div>
             <span class="error">
-                <?php if ($errMsg != "")
+                <?php if (!empty($errMsg))
                     echo $errMsg; ?>
             </span>
         </div>
