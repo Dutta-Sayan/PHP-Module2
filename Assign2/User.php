afbbfcc3d64729331f5acc59acb2ddffd687cdc4
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-03-05 18:27:25 +0530

    FT2-37-2: Added comments for functions, and logic for image display only on successful name input.

---
+++ Assign2/User.php
@@ -0,0 +1,57 @@
+<?php
+
+    /**
+     * This class validates the input given by user.
+     * 
+     * @param string: contains class variables fname and lname
+     * 
+     * @return object
+     */
+    class User {
+        public string $fname, $lname;
+
+        /**
+         * Class contructor which initializes the fname and lname variables.
+         * 
+         * @param string: first and last name are passed for initialization to class variables.
+         */
+        public function __construct($fname, $lname) {
+            $this->fname = $fname;
+            $this->lname = $lname;
+        }
+
+        /**
+         * Checks if the input name follows the general naming pattern.
+         * 
+         * @return string: on valid input returns the full name and on error returns the error input only.
+         */
+        public function isValid() {
+            $pattern = "/^[a-zA-Z]+$/";
+            if (!preg_match($pattern, $this->fname))
+                return $this->fname;
+            else if (!preg_match($pattern, $this->lname))
+                return $this->lname;
+            else {
+                $greetings = $this->fname." ".$this->lname;
+                return $greetings;
+            }
+        }
+
+        /**
+         * The function stores the image submitted by user in 'uploads' folder
+         * It returns the target address of stored image on successful upload.
+         * @return string
+         */
+        public function isValidImage() {
+            $file = $_FILES["image"];
+            $targetDir = "uploads/";
+            $target = $targetDir . basename($_FILES['image']['name']);
+            $tempFileName = $_FILES['image']['tmp_name'];
+            if (move_uploaded_file($tempFileName, $target)) {
+                return $target;       
+            }
+            else 
+                return "";
+        }
+    }
+?>
--- Assign2/index.php
+++ Assign2/index.php
@@ -3,19 +3,22 @@
     include 'User.php';
     if (isset($_POST["submit"])) {
         // Variables fname and lname hold the first and last name.
-        $fname = trim($_POST['fname']);
-        $lname = trim($_POST['lname']);
+        $fname = trim($_POST['fname'], " ");
+        $lname = trim($_POST['lname'], " ");
         $fullName = $_POST['fullName'];
         if (empty($fullName)) {
             // New object created.
             $user = new User($fname, $lname);
 
-           // Variable returnMsg stores the value determining if invalid entry is done in input fields.
+            // Variable returnMsg stores the value determining if invalid entry is done in input fields.
             $returnMsg = $user->isValid();
-            if ($returnMsg == 1)
-                $errMsg = "*Only alphabets allowed";
-            else if (!empty($returnMsg)) {
+            if ($returnMsg == $fname)
+                $ferrMsg = "*Only alphabets allowed";
+            else if ($returnMsg == $lname)
+                $lerrMsg = "*Only alphabets allowed";
+            else {
                 $greetings = $returnMsg;
+                $name = $fname." ".$lname;
                 if (isset($_FILES["image"])) {
                     $imgPath = $user->isValidImage();
                 }
@@ -44,27 +47,26 @@
         <div class="container">
             <h2>USER DETAILS</h2>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="input-form"enctype="multipart/form-data">
-                First Name: <input type="text" name="fname" placeholder="First Name" value="<?php echo $fname?>" maxlength=25 pattern="^[a-zA-Z]+$" required><br>
-                Last Name: <input type="text" name="lname" placeholder="Last Name" value="<?php echo $lname?>" maxlength=25 pattern="^[a-zA-Z]+$" required><br>
-                Full Name: <input class="fname" type="text" name="fullName" placeholder="Full Name" value="<?php if ($greetings != "") echo $fname . " " . $lname ?>" disabled><br>
-                <span class="error">
-                    <?php if (!empty($err)) echo $err; ?>
-                </span><br>
-                Upload your image <input class="image-input" type="file" name="image"><br>
+                First Name: <input type="text" name="fname" placeholder="Only alphabets allowed" value="<?php echo $fname?>" maxlength=25 pattern="^[a-zA-Z]+$" required><br>
+                <span class="error"><?php echo $ferrMsg; ?></span><br>
+
+                Last Name: <input type="text" name="lname" placeholder="Only alphabets allowed" value="<?php echo $lname?>" maxlength=25 pattern="^[a-zA-Z]+$" required><br>
+                <span class="error"><?php echo $lerrMsg; ?></span><br>
+
+                Full Name: <input class="fname" type="text" name="fullName" placeholder="Full Name" value="<?php echo $name ?>" disabled><br>
+                <span class="error"><?php echo $err; ?></span><br>
+
+                Upload your image <input class="image-input" type="file" name="image" accept="image/*" required><br>
                 <input class="submit-button" type="submit" name="submit" value="Submit">
             </form>
-            <div class="greetings-wrapper">
-                <span class="greetings">
-                    <?php if (!empty($greetings)) echo $greetings; ?><br>
-                    <?php if (!empty($greetings)) echo $greetings; ?><br>
-                    <img src="./<?php echo $imgPath?>" alt="">
-                </span>
+
+            <?php if (!empty($greetings)) { ?>
+            <div class="greetings">Hello
+            <?php echo $greetings; }?>
+            <div class="user-img">
+                <img src="./<?php echo $imgPath?>" alt="">
             </div>
-            <span class="error">
-                <?php if (!empty($errMsg))?>
-                <?php if (!empty($errMsg))
-                    echo $errMsg; ?>
-            </span>
+            </div> 
         </div>
     </section>
 </body>
