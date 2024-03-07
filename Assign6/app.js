8779816ff2cf4013210abbd2e762a7a8291ee88c
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-03-07 16:45:12 +0530

    FT2-37-6: Added a validation check of subject and marks input. - Handled the case if user inputs two suject and marks in the same line instead of next line.

---
+++ Assign6/app.js
@@ -0,0 +1,29 @@
+/**
+ * Checks if the input field for name is empty or not.
+ * If found empty, the form is prevented for being submitted.
+ */
+$(document).ready(function(){
+    $(".input-form").submit(function(e){
+        // To store the status if 'fname' or 'lname' is found empty, 'errStatus' is used 
+        let errStatus = true;
+        let fname = $(".fname").val();
+        let lname = $(".lname").val();
+        let fullName = $(".fullname").val();
+        let mobile = $(".mob").val();
+        if (fname.length == 0) {
+            $(".ferror").text("Can't be empty!");
+            errStatus = false;
+        }
+        else if (lname.length == 0) {
+            $(".lerror").text("Can't be empty!");
+            errStatus = false;
+        }
+        else if(!mobile.match(/(^(\+91)[0-9]{10}$)/)) {
+            $(".numErr").text("*Invalid number!");
+            errStatus = false;
+        }
+        if (errStatus == false) {
+            e.preventDefault();
+        }
+    });
+});
