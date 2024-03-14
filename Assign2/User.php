364ff8375ce25f1a4d446bc5ff134cd8dfb1950c
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-03-14 10:20:11 +0530

    FT2-37-2: Added backend validation for maximum characters check and allowing space between names.

--- Assign2/User.php
+++ Assign2/User.php
@@ -26,7 +26,7 @@
          * @return string: on valid input returns the full name and on error returns the error input only.
          */
         public function isValid() {
-            $pattern = "/^[a-zA-Z]+$/";
+            $pattern = "/^[a-zA-Z ]{1,25}$/";
             if (!preg_match($pattern, $this->fname))
                 return $this->fname;
             else if (!preg_match($pattern, $this->lname))
