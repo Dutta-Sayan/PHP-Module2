3ed5166f4394c01697ad9ae3b191adcaf076ad22
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-03-15 19:01:01 +0530

    FT2-37-Adv1: Made proper comments, code indentations and spacing.

--- AdvAssign1/ProcessApi.php
+++ AdvAssign1/ProcessApi.php
@@ -4,109 +4,122 @@ require_once "vendor/autoload.php";
 use GuzzleHttp\Client;
 
 /**
+ *
  * Takes the api url as input.
- * @param mixed 
+ * @param string $title
  * $title stores the title of each service.
+ * @param string $main_img
  * $main_img and $icons store the image and icons of each service respectively.
- * $desc and $explore store the description and explore links respectively for each service.
- * 
+ * @param string $desc
+ * Stores the description of each service.
+ * @param string $explore
+ * Stores the explore links for each service.
+ *
+ * The variables are string type arrays which store the different values
+ * regarding the services.
  */
-class ProcessApi {
-    private string $domain = "https://www.innoraft.com";
-    public string $url;
-    public $client;
-    public $title = [];
-    public $main_img = [];
-    public $icons = [];
-    public $desc = [];
-    public $explore = [];
+class ProcessApi
+{
+  // Stores the domain to concatenate at the first of the relative image links.
+  private string $domain = "https://www.innoraft.com";
+  // Stores the API url.
+  public string $url;
+  public $client;
+  public $title = [];
+  public $main_img = [];
+  public $icons = [];
+  public $desc = [];
+  public $explore = [];
 
-    /**
-     * Initialises the main api url passed.
-     * Creates a new Client() object.
-     * 
-     * @param string $url
-     * 
-     * @return object of ProcessApi class
-     * 
-     */
-    public function __construct(string $url) {
-        $this->url = $url;
-        $this->client = new Client();
+  /**
+   * Initialises the main api url passed.
+   * Creates a new Client() object.
+   *
+   * @param string $url
+   * The API url is passed.
+   * @return object
+   * Object of ProcessApi class
+   */
+  public function __construct(string $url) {
+    $this->url = $url;
+    $this->client = new Client();
+  }
+
+  /**
+   * Takes the api url as input and returns the PHP array which was encoded in json format.
+   * @param string $url
+   *
+   * @return array
+   */
+  public function fetchData($url) {
+    // Stores the response from API url as json data.
+    $response = $this->client->request("GET", $url);
+    // Converts the json data into PHP array.
+    return json_decode($response->getBody(), true);
+  }
+
+  /**
+   * Processes the api values and extracts the required values.
+   * Stores the values in the corresponding arrays.
+   * The value of title, image path, description, icon path and explore links are fetched.
+   */
+  public function process() {
+    // Stores the returned decoded json data as array.
+    $result = $this->fetchData($this->url);
+
+    for ($i = 12; $i <= 15; $i++) {
+      $dataAttr = $result['data'][$i]['attributes'];
+      $dataRelations = $result['data'][$i]['relationships'];
+      // Accessing the description lists.
+      $fieldService = $dataAttr['field_services']['value'];
+      // Accessing the explore links.
+      $exploreLink = $dataAttr['path']['alias'];
+
+      // Acessing the main images.
+      $imgResponseResult = $this->fetchData($dataRelations['field_image']['links']['related']['href']);
+      $img = $imgResponseResult['data']['attributes']['uri']['url'];
+      // Accessing the icons.
+      $dataRelationsData = $this->fetchData($dataRelations['field_service_icon']['links']['related']['href']);
+
+      // Array to temporarily store icons of each service.
+      $iconArr = [];
+      // Accessing icons for a single service.
+      foreach ($dataRelationsData['data'] as $iconData) {
+        $iconDataApi = $iconData['relationships']['field_media_image']['links']['related']['href'];
+        $iconDataResult = $this->fetchData($iconDataApi);
+        $icon = $iconDataResult['data']['attributes']['uri']['url'];
+        array_push($iconArr, "$this->domain.$icon");
+      }
+
+      /**
+       * The first service values is present at the last location of 'data' field.
+       * Hence checking if the 'data' field is at last value,then pushing the corresponding
+       * required values at the first of the array.
+       */
+      if ($i == 15) {
+        // Accessing and storing the titles.
+        array_unshift($this->title, $dataAttr['field_secondary_title']['value']);
+        // Storing the description lists.
+        array_unshift($this->desc, $fieldService);
+        // Storing Explore more links.
+        array_unshift($this->explore, "$this->domain.$exploreLink");
+        // Storing the main images.
+        array_unshift($this->main_img, "$this->domain.$img");
+        // Storing the icons
+        array_unshift($this->icons, $iconArr);
+      }
+      /**
+       * Storing the others services values in array as it is, serially.
+       * The first service values arestores at the 0th index of the arrays.
+       */
+      else {
+        array_push($this->title, $dataAttr['field_secondary_title']['value']);
+        array_push($this->desc, $fieldService);
+        array_push($this->explore, "$this->domain.$exploreLink");
+        array_push($this->main_img, "$this->domain.$img");
+        array_push($this->icons, $iconArr);
+      }
     }
-
-    /**
-     * Takes the api url as input and returns the PHP array which was encoded in json format.
-     * 
-     * @param string $url
-     * 
-     * @return array 
-     * 
-     */
-    public function fetchData($url) {
-        $response = $this->client->request("GET", $url);
-        return json_decode($response->getBody(), true);
-    } 
-
-    /**
-     * Processes the api values and extracts the required values.
-     * Stores the values in the corresponding arrays.
-     * The value of title, image path, description, icon path and explore links are fetched.
-     * 
-     */
-    public function process() {
-        $result = $this->fetchData($this->url);
-
-        for ($i = 12; $i <= 15; $i++) {
-            $dataAttr = $result['data'][$i]['attributes'];
-            $dataRelations = $result['data'][$i]['relationships'];
-            // Accessing the description lists.
-            $fieldService = $dataAttr['field_services']['value'];
-            // Accessing the explore links.
-            $exploreLink = $dataAttr['path']['alias'];
-
-            // Acessing the main images.
-            $imgResponseResult = $this->fetchData($dataRelations['field_image']['links']['related']['href']);
-            $img = $imgResponseResult['data']['attributes']['uri']['url'];
-            // Accessing the icons.
-            $dataRelationsData = $this->fetchData($dataRelations['field_service_icon']['links']['related']['href']);
-            
-            // Array to temporarily store icons of each service.
-            $iconArr = [];
-            // Accessing icons for a single service.
-            foreach ($dataRelationsData['data'] as $iconData) {
-                $iconDataApi = $iconData['relationships']['field_media_image']['links']['related']['href'];
-                $iconDataResult = $this->fetchData($iconDataApi);
-                $icon = $iconDataResult['data']['attributes']['uri']['url'];
-                array_push($iconArr, "$this->domain.$icon");
-            }
-
-            /**
-             * The first service values is present at the last location of 'data' field.
-             * Hence checking if the 'data' field is at last value,then pushing the corresponding
-             * required values at the first of the array.
-             * 
-             */
-            if($i==15) {
-                // Accessing and storing the titles.
-                array_unshift($this->title, $dataAttr['field_secondary_title']['value']);
-                // Storing the description lists.
-                array_unshift($this->desc, $fieldService);
-                // Storing Explore more links.
-                array_unshift($this->explore, "$this->domain.$exploreLink");
-                // Storing the main images.
-                array_unshift($this->main_img, "$this->domain.$img");
-                // Storing the icons
-                array_unshift($this->icons, $iconArr);
-            }
-            else {
-                array_push($this->title, $dataAttr['field_secondary_title']['value']);
-                array_push($this->desc, $fieldService);
-                array_push($this->explore, "$this->domain.$exploreLink");
-                array_push($this->main_img, "$this->domain.$img");
-                array_push($this->icons, $iconArr);
-            }
-        }
-    }
+  }
 }
 ?>
--- AdvAssign1/index.php
+++ AdvAssign1/index.php
@@ -1,41 +1,54 @@
 <?php
-
-    require 'ProcessApi.php';
-    $url = 'https://www.innoraft.com/jsonapi/node/services';
-    $newProcess = new ProcessApi($url);
-    $newProcess->process();
+// Class used for processing the API urls and extracting data from it.
+require 'ProcessApi.php';
+// API url.
+$url = 'https://www.innoraft.com/jsonapi/node/services';
+// Passing the API url to the class for extracting data from it.
+$newProcess = new ProcessApi($url);
+$newProcess->process();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Advance 1</title>
     <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <?php for($i=0; $i<count($newProcess->title); $i++) {?>
-            <div class="services <?php if($i%2==0) echo "odd"; ?>">
-                <div class="title">
-                    <?php echo $newProcess->title[$i];?>
-                    <div class="icons">
-                        <?php for($j=0; $j<count($newProcess->icons[$i]); $j++){?>
-                            <img class="service-icon" src="<?php echo $newProcess->icons[$i][$j];?>" alt="">
-                        <?php }?>
-                    </div>
+  </head>
 
-                    <div class="services-desc">
-                        <?php echo $newProcess->desc[$i]; ?>
-                        <a class="explore" href="<?php echo $newProcess->explore[$i]; ?>"><strong>Explore</strong></a>
-                    </div>
-                </div>
-                <div class="image">
-                    <img src="<?php echo $newProcess->main_img[$i]?>" alt="">
-                </div>
+  <body>
+    <div class="container">
+      <!-- div block for each service, displaying the services which are
+      displayed using loop.
+      Loops through the title array and display each title and its respective
+      contents. -->
+      <?php for ($i = 0; $i < count($newProcess->title); $i++) { ?>
+        <div class="services <?php if ($i % 2 == 0)
+          echo "odd"; ?>">
+          <div class="title">
+            <?php echo $newProcess->title[$i]; ?>
+            <!-- div block for service icons, each title has multiple icons
+            under it. -->
+            <div class="icons">
+              <?php for ($j = 0; $j < count($newProcess->icons[$i]); $j++) { ?>
+                <img class="service-icon"
+                  src="<?php echo $newProcess->icons[$i][$j]; ?>" alt="">
+              <?php }?>
             </div>
-                <?php }?>
+            <!-- div block for description ofeach service. -->
+            <div class="services-desc">
+              <?php echo $newProcess->desc[$i]; ?>
+              <a class="explore"
+                href="<?php echo $newProcess->explore[$i]; ?>"><strong>Explore</strong></a>
+            </div>
+          </div>
+
+          <div class="image">
+            <img src="<?php echo $newProcess->main_img[$i] ?>" alt="">
+          </div>
+        </div>
+      <?php }?>
     </div>
-</body>
+  </body>
 </html>
